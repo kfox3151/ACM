3f77d97bceff67b925036b9834cd919b5600c4ef
FoxKat <kfox3151@gmail.com>
2017-06-17 18:08:06 +0100

    BLAH
    Warning: Line endings have changed from LF to CRLF...

---
+++ ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //Arange
+            Customer customer = new Customer();
+            customer.FirstName = "Bilbo";
+            customer.LastName = "Baggins";
+
+            string expected = "Baggins, Bilbo";
+            //Act
+            string actual = customer.FullName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            Customer customer = new Customer();
+            customer.FirstName = "Bilbo";
+            string expected = "Bilbo";
+
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            Customer customer = new Customer();
+            customer.LastName = "Baggins";
+            string expected = "Baggins";
+
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
