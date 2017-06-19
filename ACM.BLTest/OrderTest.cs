869b6a05357b38cdae61a15adca363301dd0f4d7
FoxKat <kfox3151@gmail.com>
2017-06-19 14:38:43 +0100

    Added Order Class.

---
+++ ACM.BLTest/OrderTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class OrderTest
+    {
+        [TestMethod]
+        public void ValidateValid()
+        {
+            Order order = new Order();
+            order.OrderDate = DateTimeOffset.Now;
+
+            var expected = true;
+
+            var actual = order.Validate();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void ValidateMissingOrderDate()
+        {
+            Order order = new Order();
+
+            var expected = false;
+
+            var actual = order.Validate();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
