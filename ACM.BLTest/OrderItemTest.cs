31cd72e70c32c55568d284e90ad8b113827d785e
FoxKat <kfox3151@gmail.com>
2017-06-19 15:45:56 +0100

    Added OrderItem Class

---
+++ ACM.BLTest/OrderItemTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class OrderItemTest
+    {
+        [TestMethod]
+        public void ValidateValid()
+        {
+            OrderItem orderItem = new OrderItem();
+            orderItem.PurchacePrice = 50;
+            orderItem.OrderQuantity = 3;
+            orderItem.ProductId = 1;
+
+            var expected = true;
+
+            var actual = orderItem.Validate();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateMissingPurchacePrice()
+        {
+            OrderItem orderItem = new OrderItem();
+            orderItem.OrderQuantity = 5;
+            orderItem.ProductId = 1;
+
+            var expected = false;
+
+            var actual = orderItem.Validate();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateMissingOrderQuantity()
+        {
+            OrderItem orderItem = new OrderItem();
+            orderItem.PurchacePrice = 4;
+            orderItem.ProductId = 1;
+
+            var expected = false;
+
+            var actual = orderItem.Validate();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateMissingProductId()
+        {
+            OrderItem orderItem = new OrderItem();
+            orderItem.OrderQuantity = 5;
+            orderItem.PurchacePrice = 5;
+
+            var expected = false;
+
+            var actual = orderItem.Validate();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
